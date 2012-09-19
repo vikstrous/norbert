037a14653caf28a6409a76533d0c677516dc24a6
Joshua Hartman <jhartman@linkedin.com>
2012-09-19 11:54:06 -0700

    0.6.28-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object BuildSettings {
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization := "com.linkedin",
-    version      := "0.6.27-SNAPSHOT",
+    version      := "0.6.28-SNAPSHOT",
     scalaVersion := "2.8.1",
     credentialsSetting,
     publishArtifact in (Compile, packageDoc) := false,
